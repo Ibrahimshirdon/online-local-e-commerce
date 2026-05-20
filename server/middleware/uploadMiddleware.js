2a81177da5f74e8283c61a831b78a1a2b509e41b
ibra090shirdon@gmail.com <ibra090shirdon@gmail.com>
2026-05-20 21:32:59 +0300

    Allow all image extensions

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -4,15 +4,13 @@ const path = require('path');
 const storage = multer.memoryStorage();
 
 function checkFileType(file, cb) {
-    // Allow all common image formats
-    const filetypes = /jpg|jpeg|png|gif|bmp|webp|svg|tiff|tif|ico|heic|heif/;
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = /^image\//.test(file.mimetype); // Accept any image MIME type
+    // Accept any image MIME type, regardless of the file extension
+    const mimetype = /^image\//.test(file.mimetype);
 
-    if (extname && mimetype) {
+    if (mimetype) {
         return cb(null, true);
     }
-    cb('Images only!');
+    cb(new Error('Images only!'));
 }
 
 const upload = multer({
